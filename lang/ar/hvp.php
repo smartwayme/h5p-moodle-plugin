38497ec19ec217922fa3c479a6cff0f60303838e
smartwayme <39260358+smartwayme@users.noreply.github.com>
2018-07-03 11:58:45 +0300

    Create hvp.php
    Added Arabic Translation for the moodle plugin component

---
+++ lang/ar/hvp.php
@@ -0,0 +1,300 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['modulename'] = 'محتوى تفاعلي';
+$string['modulename_help'] = 'هذا النموذج يمكنك من انشاء محتوى تفاعلي مثل فيديو تفاعلي أو مجموعة أسئلة أو مجموعة اس~لة عن طريق السحب والاافلات أو الاسئلة ذات الخيارات المتعددة, العروض التقديمية وأكثر من ذلك.
+
+بالإضافة لكونك  توجد أدوات غنية للمحتوى و يمكنك من استيراد وتصدير ملفات فعالة لاعادة الاستخدام والمشاركة للمحتوى
+
+تفاعلات المستخدم والنتائج تتم مراقبتها باستخدام برنامج وهي متاحة ضمن كتاب مودل للتقييم
+
+يمكنك أن تضيف المحتوى بانشاء اداة مضمنة أو بتحميل ملفات موجودة في مواقع متاحة أخرى.';
+$string['modulename_link'] = 'https://h5p.org/moodle-more-help';
+$string['modulenameplural'] = 'المحتوى التفاعلي';
+$string['pluginadministration'] = 'H5P';
+$string['pluginname'] = 'H5P';
+$string['intro'] = 'المقدمة';
+$string['h5pfile'] = 'ملف';
+$string['fullscreen'] = 'ملء الشاشة';
+$string['disablefullscreen'] = 'الغاء ملء الشاشة';
+$string['download'] = 'تحميل';
+$string['copyright'] = 'حقوق الاستخدام';
+$string['embed'] = 'مضمن';
+$string['showadvanced'] = 'عرض متقدمة';
+$string['hideadvanced'] = 'إخفاء متقدمة';
+$string['resizescript'] = 'تضمين هذا النص على موقعك الويب اذا كنت تريد تغيير الحجم الديناميكي للمحتوى المضمن';
+$string['size'] = 'حجم';
+$string['close'] = 'إغلاق';
+$string['title'] = 'عنوان';
+$string['author'] = 'مؤلف';
+$string['year'] = 'عام';
+$string['source'] = 'مصدر';
+$string['license'] = 'شهادة';
+$string['thumbnail'] = 'صورة مصغرة';
+$string['nocopyright'] = 'لا تتوفر معلومات حقوق الطبع  والنشر لهذا المحتوى';
+$string['downloadtitle'] = 'تنزيل هذا المحتوى';
+$string['copyrighttitle'] = 'عرض معلومات حقوق الطبع والنشر لهذاالمحتوى';
+$string['embedtitle'] = 'عرض شيفرة التضمين لهذاالمحتوى';
+$string['h5ptitle'] = 'قم بزيارة الموقع للاطلاع على المزيد من المحتوى الرائع';
+$string['contentchanged'] = 'لقد تغير هذا المحتوى منذ آخر مرة استخدمته';
+$string['startingover'] = "عليك أن تبدأ من جديد.";
+$string['confirmdialogheader'] = 'تأكيد';
+$string['confirmdialogbody'] = 'يرجى تأكيد رغبتك في المتابعة. هذا الإجراء غير قابل للعكس';
+$string['cancellabel'] = 'الغاء';
+$string['confirmlabel'] = 'تأكيد';
+$string['noh5ps'] = 'لا يتوفر محتوى تفاعلي لهذا المنهج';
+
+$string['lookforupdates'] = 'البحث عن تحديثات';
+$string['updatelibraries'] = 'تحديث جميع المكتبات';
+$string['removetmpfiles'] = 'ازالة الملفات القديمة المؤقتة';
+$string['removeoldlogentries'] = 'ازالة سجلات الدخول القديمة';
+
+// Admin settings.
+$string['displayoptionnevershow'] = 'لا تظهر';
+$string['displayoptionalwaysshow'] = 'أظهر دائماً';
+$string['displayoptionpermissions'] = 'إظهار فقط إذا كان لدى المستخدم صلاحيات التصدير';
+$string['displayoptionauthoron'] = 'يتحكم بها المؤلف الافتراضي على التشغيل';
+$string['displayoptionauthoroff'] = 'يتحكم بها المؤلف الافتراضي هو الايقاف';
+$string['displayoptions'] = 'خصائص العرض';
+$string['enableframe'] = 'عرض شريط الإجراءات والإطار';
+$string['enabledownload'] = 'زر التحميل';
+$string['enableembed'] = 'زر التضمين';
+$string['enablecopyright'] = 'زر حقوق الطبع والنشر';
+$string['enableabout'] = 'زر حول';
+$string['hubsettingsheader'] = 'انواع المحتوى';
+$string['enablehublabel'] = 'استخدام المحور';
+$string['disablehubdescription'] = "ينصح وبشدة تمكين هذا الخيار. يوفر المحور واجهة سهلة للحصول على أنواع محتوى جديدة والحفاظ على أنواع المحتوى الموجودة حتى الآن. في المستقبل،سيكون من الأسهل مشاركة واعادة استخدام المحتوى. إذا تم تعطيل هذا الخيار، فسيتعين عليك تثبيت أنواع المحتوى وتحديثها من خلال نماذج تحميل الملفات";
+$string['empty'] = 'فارغ';
+$string['reveal'] = 'كشف';
+$string['hide'] = 'اخفاء';
+$string['sitekey'] = 'مفتاح الموقع';
+$string['sitekeydescription'] = 'مفتاح الموقع هو السر الذي يحدد هذا الموقع بشكل فريد مع المحور';
+
+$string['sendusagestatistics'] = 'مشاركة احصائيات الاستخدام';
+$string['sendusagestatistics_help'] = 'سيتم الإبلاغ عن أرقام إحصاءات الاستخدام تلقائيا لمساعدة المطورين على فهم أفضل لكيفية الاستخدام وتحديد المجالات المحتملة للتحسين.';
+$string['enablesavecontentstate'] = 'حفظ حالة المحتوى';
+$string['enablesavecontentstate_help'] = 'حفظ الحالة الحالية للمحتوى التفاعلي لكل مستخدم تلقائيا. وهذا يعني أن المستخدم قد يكمل حيث توقف.';
+$string['contentstatefrequency'] = 'حفظ تردد حالة المحتوى';
+$string['contentstatefrequency_help'] = 'في ثوان، كم مرة  ترغب المستخدم أن يحفظ العمل. زيادة هذا العدد إذا كنت تواجه مشاكل مع الطلبات الأجاكس الكثيرة';
+$string['enabledlrscontenttypes'] = 'تمكين أنواع المحتوى التابعة ل  LRS';
+$string['enabledlrscontenttypes_help'] = 'يجعل من الممكن استخدام أنواع المحتوى التي تعتمد على مخزن سجل التعلم لتعمل بشكل صحيح، مثل نوع المحتوى الاستبيان.';
+
+// Admin menu.
+$string['contenttypecacheheader'] = 'نوع المحتوى كاش';
+$string['settings'] = 'اعدادات';
+$string['libraries'] = 'مكتبات';
+
+// Content type cache section
+$string['ctcacheconnectionfailed'] = "تعذر التواصل مع المركز الرجاء معاودة المحاولة في وقت لاحق";
+$string['ctcachenolibraries'] = 'لم يتم استلام أي انواع محتوى الرجاء معاودة المحاولة لاحقاً';
+$string['ctcachesuccess'] = 'تم تعديل ذاكرة المكتبة بنجاح';
+$string['ctcachelastupdatelabel'] = 'التحديث الأخير';
+$string['ctcachebuttonlabel'] = 'تحديث ذاكرة نوع المحتوى';
+$string['ctcacheneverupdated'] = 'مطلقا';
+$string['ctcachetaskname'] = 'تحديث ذاكرة نوع المحتوى';
+$string['ctcachedescription'] = 'التأكد من أن الذاكرة  لنوع المحتوى محدثة سيضمن إمكانية عرض أحدث المكتبات وتنزيلها واستخدامها. وهذا يختلف عن تحديث المكتبات نفسها';
+
+// Upload libraries section.
+$string['uploadlibraries'] = 'رفع المكتبات';
+$string['options'] = 'خصائص';
+$string['onlyupdate'] = 'قم بتحديث المكتبات الموجودة فقط';
+$string['disablefileextensioncheck'] = 'تعطيل التحقق من امتداد الملف';
+$string['disablefileextensioncheckwarning'] = "تحذير! قد يؤدي تعطيل التحقق من إضافة الملف إلى آثار أمنية لأنه يسمح بتحميل ملفات php. وهذا بدوره قد يجعل المهاجمين قادرين على تنفيذ الشيفرات على موقعك. الرجاء التأكد من أنك تعرف بالضبط ما تقوم بتحميله";
+$string['upload'] = 'رفع';
+
+// Installed libraries section.
+$string['installedlibraries'] = 'المكتبات المثبتة';
+$string['invalidtoken'] = 'رمز أمان غير صالح';
+$string['missingparameters'] = 'معلمات مفقودة';
+$string['nocontenttype'] = 'لم يتم تحديد نوع المحتوى';
+$string['invalidcontenttype'] = 'نوع المحتوى المختار غير صالح';
+$string['installdenied'] = 'ليس لديك إذن لتثبيت أنواع المحتوى. اتصل بمشرف موقعك.';
+$string['downloadfailed'] = 'فشل تحميل المكتبة المطلوبة ';
+$string['validationfailed'] = 'المحتوى المطلوب غير صالح';
+$string['validatingh5pfailed'] = 'فشل التحقق من الحزمة';
+
+// H5P library list headers on admin page.
+$string['librarylisttitle'] = 'العنوان';
+$string['librarylistrestricted'] = 'محدد';
+$string['librarylistinstances'] = 'الحالات';
+$string['librarylistinstancedependencies'] = 'تبعيات الحالة';
+$string['librarylistlibrarydependencies'] = 'تبعيات المكتبة';
+$string['librarylistactions'] = 'أفعال';
+
+// H5P library page labels.
+$string['addlibraries'] = 'إضافة مكتبات';
+$string['installedlibraries'] = 'المكتبات المحملة';
+$string['notapplicable'] = 'N/A';
+$string['upgradelibrarycontent'] = 'ترقية محتوى المكتبة';
+
+// Upgrade H5P content page.
+$string['upgrade'] = 'ترقية';
+$string['upgradeheading'] = 'ترقية المحتوى';
+$string['upgradenoavailableupgrades'] = 'لا توجد ترقيات متاحة لهذه المكتبة.';
+$string['enablejavascript'] = 'يرجى تمكين جافا سكريبت';
+$string['upgrademessage'] = 'أنت على وشك ترقية نسخة المحتوى. الرجاء تحديد إصدار الترقية.';
+$string['upgradeinprogress'] = 'جار الترقية الى ..';
+$string['upgradeerror'] = 'حدث خطأ أثناء معالجة المعلمات';
+$string['upgradeerrordata'] = 'تعذر تحميل بيانات المكتبة';
+$string['upgradeerrorscript'] = 'تعذر تحميل نص سكريبت لترقيات';
+$string['upgradeerrorcontent'] = 'تعذر ترقية المحتوى';
+$string['upgradeerrorparamsbroken'] = 'تم تقسيم المعلمات';
+$string['upgradedone'] = 'لقد قمت بنجاح بترقية نسخة المحتوى';
+$string['upgradereturn'] = 'اعادة';
+$string['upgradenothingtodo'] = "ليس هناك نسخ محتوى للترقية";
+$string['upgradebuttonlabel'] = 'ترقية';
+$string['upgradeinvalidtoken'] = 'خطأ: رمز الأمان غير صالح!';
+$string['upgradelibrarymissing'] = 'خطأ: مكتبتك مفقودة!';
+
+// Results / report page.
+$string['user'] = 'مستخدم';
+$string['score'] = 'نتيجة';
+$string['maxscore'] = 'النتيجة العظمى';
+$string['finished'] = 'منتهي';
+$string['loadingdata'] = 'تحميل البيانات';
+$string['ajaxfailed'] = 'فشل في تحميل البيانات';
+$string['nodata'] = "لا تتوفر أية بيانات تطابق معاييرك.";
+$string['currentpage'] = 'الصفحة $الحالية من $الكلي';
+$string['nextpage'] = 'الصفحة التالية';
+$string['previouspage'] = 'الصفحة السابقة';
+$string['search'] = 'بحث';
+$string['empty'] = 'لا يوجد نتائج متوفرة';
+
+// Editor
+$string['javascriptloading'] = 'انتظار جافا سكريبت ...';
+$string['action'] = 'أفعال';
+$string['upload'] = 'رفع';
+$string['create'] = 'انشاء';
+$string['editor'] = 'محرر';
+
+$string['invalidlibrary'] = 'مكتبة غير صالحة';
+$string['nosuchlibrary'] = 'لا توجد مثل هذه المكتبة';
+$string['noparameters'] = 'لا معلمات';
+$string['invalidparameters'] = 'معلمات غير صالحة';
+$string['missingcontentuserdata'] = 'خطأ: تعذر العثور على بيانات مستخدم المحتوى';
+
+$string['maximumgrade'] = 'الدرجة العظمى';
+$string['maximumgradeerror'] = 'يرجى إدخال عدد صحيح موجب صالح كنقاط الحد الأقصى المتاحة لهذا النشاط';
+
+// Capabilities
+$string['hvp:addinstance'] = 'اضافة نشاط جديد';
+$string['hvp:restrictlibraries'] = 'تحديد المكتبة';
+$string['hvp:updatelibraries'] = 'قم بتحديث إصدار المكتبة';
+$string['hvp:userestrictedlibraries'] = 'استخدام المكتبات المقيدة';
+$string['hvp:savecontentuserdata'] = 'حفظ بيانات مستخدم المحتوى';
+$string['hvp:saveresults'] = 'حفظ نتيجة المحتوى';
+$string['hvp:viewresults'] = 'عرض نتيجة المحتوى';
+$string['hvp:getcachedassets'] = 'الحصول على مواد عرض المحتوى المخزنة';
+$string['hvp:getcontent'] = 'الحصول على/ عرض محتوى الملف في المقرر';
+$string['hvp:getexport'] = 'الحصول على ملف التصدير من المقرر';
+$string['hvp:installrecommendedh5plibraries'] = 'تثبيت المكتبات الموصى بها';
+
+// Capabilities error messages
+$string['nopermissiontoupgrade'] = 'لا تملك الصلاحية لترقية المكتبات';
+$string['nopermissiontorestrict'] = 'ليس لديك إذن بتقييد المكتبات.';
+$string['nopermissiontosavecontentuserdata'] = 'ليس لديك إذن لحفظ بيانات مستخدم المحتوى.';
+$string['nopermissiontosaveresult'] = 'ليس لديك إذن لحفظ نتيجة لهذا المحتوى.';
+$string['nopermissiontoviewresult'] = 'ليس لديك إذن لعرض نتائج لهذا المحتوى.';
+
+// Editor translations
+$string['noziparchive'] = 'اصدار ال بي اتش بي الخاص بك لا يدعم الارشيف المضغوط';
+$string['noextension'] = 'الملف الذي قمت بتحميله ليس حزمة اتش تي ام ال صالحة لا يملك امتدااد الملف';
+$string['nounzip'] = 'الملف الذي قمت بتحميله ليس حزمة اتش تي ام ال صالحة تعذر فك الضغط';
+$string['noparse'] = 'تعذر تحليل الملف الرئيسي';
+$string['nojson'] = 'الملف الرئيسي غير صالح';
+$string['invalidcontentfolder'] = 'مجلد محتوى غير صالح';
+$string['nocontent'] = 'تعذر العثور على الملف او تحليله';
+$string['librarydirectoryerror'] = 'يجب ان يتطابق اسم دليل المكتبة مع  اسم الجهاز او اسم الجهاز النسخة الرئيسية او النسخة الثانويةالامتداد امتداد النسخة الرئيسية  امتداد النسخة الثانوية ';
+$string['missingcontentfolder'] = 'هناك مجلد محتوى صالح مفقود';
+$string['invalidmainjson'] = 'الملف الرئيسي مفقود';
+$string['missinglibrary'] = 'المكتبة المطلوبة مفقودة {$a->@library}';
+$string['missinguploadpermissions'] = "لاحظ أن المكتبات قد تكون موجودة في الملف الذي حملته، ولكن لا يسمح لك بتحميل مكتبات جديدة. اتصل بمسؤول الموقع حول هذا الموضوع";
+$string['invalidlibraryname'] = 'اسم المكتبة غير صالح {$a->%name}';
+$string['missinglibraryjson'] = 'تعذر العثور على الملف بالتنسيق الملائم للمكتبة {$a->%name}';
+$string['invalidsemanticsjson'] = 'تم تضمين ملف غير صالح في المكتبة {$a->%name}';
+$string['invalidlanguagefile'] = 'ملف اللغة غير صالح {$a->%file} في المكتبة {$a->%library}';
+$string['invalidlanguagefile2'] = 'ملف لغة غير صالح {$a->%languageFile} تم تضمينه في المكتبة {$a->%name}';
+$string['missinglibraryfile'] = 'الملف "{$a->%file}" مفقود من المكتبة "{$a->%name}"';
+$string['missingcoreversion'] = 'النظام غير قادر على تحميل <em>{$a->%component}</em> المكون من الحزمة يتطلب اصدار احدث من البرنامج المساعد هذا الموقع قيد التشغيل حاليا {$a->%current}, حيث الاصدار المطلوب {$a->%required} أو أعلى. يجب التفكير في الترقية ثم اعادة المحاولة';
+$string['invalidlibrarydataboolean'] = 'البيانات المتوفرة غير صالحة {$a->%property} في {$a->%library}. المتوقع قيمة منطقية.';
+$string['invalidlibrarydata'] = 'تم توفير بيانات غير  صالحة {$a->%property} في {$a->%library}';
+$string['invalidlibraryproperty'] = 'لا يمكن قراءة الخاصية {$a->%property} في {$a->%library}';
+$string['missinglibraryproperty'] = 'الخاصية المطلوبة {$a->%property} مفقودة من {$a->%library}';
+$string['invalidlibraryoption'] = 'خيار غير قانوني {$a->%option} في {$a->%library}';
+$string['addedandupdatelibraries'] = 'تم اضافة {$a->%new} مكتبات جديدة وتحديث {$a->%old} القديمة.';
+$string['addednewlibraries'] = 'تم اضافة {$a->%new} مكتبات جديدة';
+$string['updatedlibraries'] = 'تم تعديل {$a->%old} مكتبات.';
+$string['missingdependency'] = 'التبعية مفقودة {$a->@dep} مطلوبة من {$a->@lib}.';
+$string['invalidstring'] = 'النص المقدم غير صالح وفقا لتكرار الدلالات  (القيمة: \"{$a->%value}\", الدلالة: \"{$a->%regexp}\")';
+$string['invalidfile'] = 'الملف "{$a->%filename}" غير مسموح. الملفات بالامتدادت التالية هي المسموحة فقط : {$a->%files-allowed}.';
+$string['invalidmultiselectoption'] = 'الاختيار المحدد لا يمكن ان يدعم الاختيار المتعدد';
+$string['invalidselectoption'] = 'الخيار المحدد غير صالح للختيار';
+$string['invalidsemanticstype'] = 'أخطاء داخلية: نوع محتوى غير معروف "{$a->@type}" في الدلالات. إزالة محتوى';
+$string['copyrightinfo'] = 'معلومات حقوق النشر';
+$string['years'] = 'سنوات';
+$string['undisclosed'] = 'غير معلوم';
+$string['attribution'] = 'ميزات 4.0';
+$string['attributionsa'] = 'ميزات - مشاركة متساوية 4.0';
+$string['attributionnd'] = 'Attribution-NoDerivs 4.0';
+$string['attributionnc'] = 'ميزات- غير تجارية 4.0';
+$string['attributionncsa'] = 'ميزات - مشاركة متساوية - غير تجارية 4.0';
+$string['attributionncnd'] = 'Attribution-NonCommercial-NoDerivs 4.0';
+$string['gpl'] = 'الترخيص العام v3';
+$string['pd'] = 'المجال العام';
+$string['pddl'] = 'وقف تخصيص المجال العام والترخيص';
+$string['pdm'] = 'علامة النطاق العام';
+$string['copyrightstring'] = 'حقوق النشر';
+$string['unabletocreatedir'] = 'غير قادر على إنشاء دليل';
+$string['unabletogetfieldtype'] = 'تعذر الحصول على نوع الحقل';
+$string['filetypenotallowed'] = 'نوع الملف غير مسموح بها';
+$string['invalidfieldtype'] = 'نوع الحقل غير صالح';
+$string['invalidimageformat'] = 'تنسيق ملف الصورة غير صالح استخدم تنسيق jpg ,png أو gif';
+$string['filenotimage'] = 'الملف ليس صورة';
+$string['invalidaudioformat'] = 'نوع ملف صوتي غير صالح استخدم mp3 أو wav';
+$string['invalidvideoformat'] = 'نوع ملف الفيديو غير صالح. Use mp4 أو webm.';
+$string['couldnotsave'] = 'لا يمكن حفظ الملف';
+$string['couldnotcopy'] = 'لا يمكن نسخ الملف';
+
+// Welcome messages
+$string['welcomeheader'] = 'مرحبا بك في عالمنا';
+$string['welcomegettingstarted'] = 'للبدء قم بزيارة  <a {$a->moodle_tutorial}>tutorial</a> وقم بزيارة  <a {$a->example_content}>example content</a> at H5P.org لأخذ نبذة';
+$string['welcomecommunity'] = 'نأمل أن تستمع وتحصل على المشاركة في مجتمعنا المتنامي من خلال موقعنا <a {$a->forums}>forums</a> وغرفة المحادثة <a {$a->gitter}>H5P at Gitter</a>';
+$string['welcomecontactus'] = 'اذا كان لديك أي ملاحظات لا تتردد في  <a {$a}>contact us</a>. نحن نأخذ ردود الفعل على محمل الجد ومكرسة لجعل نظامنا أفضل كل يوم!';
+$string['missingmbstring'] = 'لم يتم تحديد امتداد نص بي اتش بي. نحتاجه للعمل بالشكل المناسب';
+$string['wrongversion'] = 'اصدار المكتبة {$a->%machineName} المستخدم في هذا المحتوى غير صالح. المحتوى  يحوي {$a->%contentLibrary}, لكن يجب أن يكون {$a->%semanticsLibrary}.';
+$string['invalidlibrary'] = 'المكتبة {$a->%library}المستخدمة في المحتوى غير صالحة';
+
+// Setup errors
+$string['oldphpversion'] = 'إصدارك ال بي اتش بي قديم. يحتاج إصدار 5.2 ليعمل بالشكل المناسب. نسخة 5.6 أو الأحدث هي الأفضل';
+$string['maxuploadsizetoosmall'] = 'حجم التحميل الأعظمي صغير جدا مع الاعدادات الحالية لا يمكنك تحميل ملفات أكبر من  {$a->%number} MB. قد يكون هناك مشكلة في محاولة تحميل الصور أو مقاطع الفيديو يرجى زيادة الحجم لأكثر من 5MB';
+$string['maxpostsizetoosmall'] = 'حجم البريد الاعظمي صغير جداً. مع اعدادك الحالي, لا يمكنك رفع ملفات اكبر من  {$a->%number} MB.يمكن أن يسبب ذلك مشكلة عند رفع, الصور ومقاطع الفيديو. الرجاء النظر في زيادة الحجم لأعلى من 5MB';
+$string['sslnotenabled'] = 'السيرفر لا يحوي اس اس ال  ممكنة.  ينبغي تمكينها لضمان اتصال آمن مع المحور';
+$string['hubcommunicationdisabled'] = 'تم تعطيل اتصال المحور بسبب فشل واحد أو أكثر من المتطلبات.';
+$string['reviseserversetupandretry'] = 'عند الانتهاء من مراجعة اعدادات الخادم يمكنك إعادة تمكين اتصال المحور في الإعدادات';
+$string['disablehubconfirmationmsg'] = 'هل لا تزال تريد تمكين المحور ?';
+$string['nowriteaccess'] = 'تم الكشف عن مشكلة في الوصول إلى الكتابة على الخادم يرجى التأكد من أن الخادم الخاص بك يمكن أن يكتب إلى مجلد البيانات الخاصة بك.';
+$string['uploadsizelargerthanpostsize'] = 'حجم التحميل الاعظمي أكبر من حجم المشاركة الاعظمي ومن المعروف أن هذا يسبب مشاكل في بعض التنزيلات';
+$string['sitecouldnotberegistered'] = 'تعذر تسجيل الموقع في المحور. يرجى الاتصال بمسؤول الموقع.';
+$string['hubisdisableduploadlibraries'] = 'تم تعطيل المحور حتى يتم حل هذه المشكلة. لا يزال بإمكانك تحميل مكتبات من خلال صفحة "المكتبات".';
+$string['successfullyregisteredwithhub'] = 'تم تسجيل موقعك بنجاح في  المحور.';
+$string['sitekeyregistered'] = 'لقد تم تزويدك بمفتاح فريد يحدد لك المحور عند تلقي تحديثات جديدة. المفتاح متاح للعرض في صفحة الاعدادات.';
+
+// Ajax messages
+$string['hubisdisabled'] = 'تم تعطيل المحور يمكنك إعادة تمكينه من الاعدادات';
+$string['invalidh5ppost'] = 'تعذر الحصول على النظام المنشور';
+$string['filenotfoundonserver'] = 'لم يتم العثور على الملف على الخادم. تحقق من إعدادات تحميل الملف.';
+$string['failedtodownloadh5p'] = 'إخفاق في تنزيل الملف المطلوب';
+$string['postmessagerequired'] = 'مطلوب رسالة بريدية للوصول إلى نقطة النهاية المحددة';
